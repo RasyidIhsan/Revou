fa5117b8ca47dabc41d25c841bfc1b89aed9c43f
RasyidIhsan <rasyidselian09@gmail.com>
2024-05-23 15:06:10 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+
+function Calculate(){
+    var height = document.getElementById("h-input").value;
+    var weight = document.getElementById("w-input").value;
+
+    var result = parseFloat(weight) /(parseFloat(height)/100)**2;
+
+    if(!isNaN(result)){
+        document.getElementById("bmi-output").innerHTML = result;
+        if(result < 18.5){
+            document.getElementById("bmi-status").innerHTML = "Kekurangan berat badan";
+        }
+        else if(result  < 24.9){
+            document.getElementById("bmi-status").innerHTML = "Normal (Ideal)";
+        }
+        else if(result < 29.9){
+            document.getElementById("bmi-status").innerHTML = "Kelebihan berat badan (Overweight)";
+        }
+        else{
+            document.getElementById("bmi-status").innerHTML = "Kegemukan (Obesitas)";
+        }
+    }
+}
